8b4975d6065c0f05d13ecb4944920142a91daac1
Anna Tatum <purelle626@gmail.com>
2021-04-15 23:20:30 -0500

    Added login page

---
+++ src/main/java/org/launchcode/ministrytracker/controllers/MvcConfig.java
@@ -0,0 +1,17 @@
+package org.launchcode.ministrytracker.controllers;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class MvcConfig implements WebMvcConfigurer{
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/index").setViewName("index");
+        //registry.addViewController("/").setViewName("index");
+        registry.addViewController("/members").setViewName("members");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
