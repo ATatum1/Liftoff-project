6314535de98003f2103312adf2f72fc4743fbcb2
Anna Tatum <purelle626@gmail.com>
2021-04-26 17:27:11 -0500

    removed todo check boxes

--- src/main/java/org/launchcode/ministrytracker/controllers/Registration.java
+++ src/main/java/org/launchcode/ministrytracker/controllers/Registration.java
@@ -3,14 +3,8 @@ package org.launchcode.ministrytracker.controllers;
 import org.launchcode.ministrytracker.models.dto.UserDto;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.ModelAndView;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.validation.Valid;
 
 @Controller
 public class Registration {
@@ -23,8 +17,8 @@ public class Registration {
     }
 
 
-    public ModelAndView registerUserAccount(@ModelAttribute("user") @Valid UserDto userDto,
-                                            HttpServletRequest request, Errors errors) {
-        //...
-    }
+//    public ModelAndView registerUserAccount(@ModelAttribute("user") @Valid UserDto userDto,
+//                                            HttpServletRequest request, Errors errors) {
+//        //...
+//    }
 }
